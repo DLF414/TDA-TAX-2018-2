56f92b3a4282afdf573b104c2f40373759dc91b1
DLF414 <Dmitry220198@gmail.com>
2018-05-21 06:34:25 +0300

    controllers update

--- controllers/account.js
+++ controllers/account.js
@@ -10,65 +10,111 @@ class AccountController extends CrudController{
         this.routes = {
             '/': [
                 { method: 'get', cb: this.readAll },
-            ],
-            '/:login': [
-                { method: 'get', cb: this.readlogin },
-                { method: 'put', cb: this.updatelogin },
-                { method: 'delete', cb: this.deletelogin }
-            ],
-            '/:id': [
-                { method: 'get', cb: this.readid },
-                { method: 'put', cb: this.updateid },
-                { method: 'delete', cb: this.deleteid }
             ]
         };
 
         this.registerRoutes();
     }
 
-    async readlogin(req, res){
-        let data = await this.service.readByLogin(req.params.login);
-        res.json(data);
-    }
 
-    async deletelogin(req, res){
-        const user = await this.service.readByLogin(req.params.login);
-        const checkValue = await checkAuth(req.ability, 'delete', user);
-        if(checkValue.access){
-            await this.service.delete(user.id);
+    async readAll(req, res) {
+        switch (req.query.op) {
+            case 'logout':
+                if (!req.body.auth.logged) throw this.service.errors.InsufficientAccountPermissions;
+                res.cookie('_token', '', {
+                    maxAge: 0
+                });
+                res.redirect(req.headers.referer);
+                break;
+            case 'signup':
+                if (req.body.auth.logged) throw this.service.errors.InsufficientAccountPermissions;
+                res.render('regform');
+                break;
+            case 'signin':
+                if (req.body.auth.logged) throw this.service.errors.InsufficientAccountPermissions;
+                let user_info, block;
+                if(!req.query.login || !req.query.password) throw this.service.errors.MissingRequiredQueryParameter;
+                user_info = await this.service.signinUser(req.query.login, req.query.password);
+                if (!user_info) throw this.service.errors.AuthenticationFailed;
+                let token = jwt.sign({
+                        _id: user_info.id,
+                        admin: user_info.is_admin,
+                        moderator: user_info.is_moderator
+                    },
+                    config.jwt.secret, {
+                        expiresIn: 86400
+                    });
+                res.cookie('_token', token, {
+                    maxAge: 86400000
+                })
+                res.redirect(req.headers.referer);
+                break;
+            default:
+                if (!req.body.auth.payload.admin && !req.body.auth.payload.moderator)
+                    throw this.service.errors.InsufficientAccountPermissions;
+                res.json(await this.service.readNE());
+                break;
         }
-        else{
-            throw checkValue.error;
+
+    }
+
+    async read(req, res) {
+        switch (req.query.op) {
+            case 'confirm':
+                if (req.body.auth.logged) throw this.service.errors.InsufficientAccountPermissions;
+                if (await this.service.isActual(req.query.verify)) {
+                    await this.service.confirm(req.params.id, req.query.verify);
+                    res.redirect(`/user/${req.params.id}?op=continue_signup`);
+                } else {
+                    await this.service.delete(req.params.id);
+                    throw this.service.errors.InvalidQueryParameterValue;
+                }
+                break;
+            case 'continue_signup':
+                if (req.body.auth.logged) throw this.service.errors.InsufficientAccountPermissions;
+                res.render('regform2', {
+                    id: req.params.id
+                });
+                break;
+            case 'message':
+                if (req.body.auth.logged) throw this.service.errors.InsufficientAccountPermissions;
+                res.render('confirm');
+                break;
+            default:
+                let account = await this.service.read(req.params.id);
+
+                res.render('user', {
+                    user: account,
+                    auth: req.body.auth,
+                    emails: { in: emails_in,
+                        out: emails_out
+                    }
+                });
+                break;
         }
-        res.sendStatus(200);
+
     }
 
-    async updatelogin(req, res){
-        const user = await this.service.readByLogin(req.params.login);
-        const checkValue = await checkAuth(req.ability, 'update', user);
-        if(checkValue.access){
-            await this.service.update(user.id, req.body);
-        }
-        else{
-            throw checkValue.error;
-        }
-        res.sendStatus(200);
+    async update(req, res) {
+
+        if (req.body.auth.logged)
+            if (req.body.auth.payload._id != req.params.id) throw this.service.errors.InsufficientAccountPermissions;
+        await this.service.update(req.body, req.params.id);
+        res.status(200);
+        res.end();
     }
 
-    async readid(req, res) {
-        let data = await this.service.read(req.params.id);
-        res.json(data);
+    async create(req, res) {
+        if (req.body.auth.logged) throw this.service.errors.InsufficientAccountPermissions;
+        if (!(req.body.signup_login && req.body.signup_password)) throw this.service.errors.InvalidInput;
+        let check = await this.service.readNE();
+        if (req.body.signup_password.length < 8) throw this.service.errors.InvalidInput;
+        if (check.users.includes(req.body.signup_login) ) throw this.service.errors.AccountAlreadyExists;
+        await this.service.create(req.body, req.headers.host);
+        res.status(200);
+        res.end();
     }
 
-    async updateid(req, res) {
-        let data = await this.service.update(req.params.id, req.body);
-        res.json(data);
-    }
-
-    async deleteid(req, res) {
-        let data =  await this.service.delete(req.params.id);
-        res.json(data);
-    }
 }
 
 module.exports = (accountService) => {
--- controllers/crud.js
+++ controllers/crud.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const wrap = require('../helpers/wrap');
+const express = require("express");
+const wrap = require("../helpers/wrap");
 
 class CrudController {
     constructor(service) {
@@ -11,56 +11,56 @@ class CrudController {
         this.update = this.update.bind(this);
         this.delete = this.delete.bind(this);
 
-        this.router = express.Router({mergeParams: true});
+        this.router = express.Router();
         this.routes = {
-            '/': [
-                { method: 'get', cb: this.readAll },
-                { method: 'post', cb: this.create }
+            "/": [
+                { method: "get", cb: this.readAll },
+                { method: "post", cb: this.create }
             ],
-            '/:id': [
-                { method: 'get', cb: this.read },
-                { method: 'put', cb: this.update },
-                { method: 'delete', cb: this.delete }
+            "/:id": [
+                { method: "get", cb: this.read },
+                { method: "put", cb: this.update },
+                { method: "delete", cb: this.delete }
             ]
         };
+        this.registerRoutes();
     }
 
     async readAll(req, res) {
-        let data = await this.service.readChunk(req.query);
-        res.json(data);
+        if(!req.body.auth.payload.admin) throw this.service.errors.InsufficientAccountPermissions;
+        res.json(await this.service.readChunk());
     }
-
     async read(req, res) {
-        let data = await this.service.read(req.params.id);
-        res.json(data);
+        if(!req.body.auth.payload.admin) throw this.service.errors.InsufficientAccountPermissions;
+        res.json(await this.service.read(req.params.id));
     }
-
     async create(req, res) {
-        let data = await this.service.create(req.body);
-        res.json(data);
+        if(!req.body.auth.payload.admin) throw this.service.errors.InsufficientAccountPermissions;
+        const properties = await this.service.update(req.body);
+        res.json(properties);
     }
-
     async update(req, res) {
-        let data = await this.service.update(req.params.id, req.body);
-        res.json(data);
+        if(!req.body.auth.payload.admin) throw this.service.errors.InsufficientAccountPermissions;
+        req.body.id = req.params.id;
+        const properties = await this.service.update(req.body);
+        res.json(properties);
     }
-
     async delete(req, res) {
-        let data =  await this.service.delete(req.params.id);
-        res.json(data);
+        if(!req.body.auth.payload.admin) throw this.service.errors.InsufficientAccountPermissions;
+        const properties = await this.service.delete(req.params.id);
+        res.json(properties);
     }
 
     registerRoutes() {
         Object.keys(this.routes).forEach(route => {
             let handlers = this.routes[route];
+
             if (!handlers || !Array.isArray(handlers)) {
                 return;
             }
+
             for (let handler of handlers) {
-                this.router[handler.method](
-                    route,
-                    wrap(handler.cb)
-                );
+                this.router[handler.method](route, wrap(handler.cb));
             }
         });
     }
