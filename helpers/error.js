029af95b157568806b214c497aa602e9b1803a6d
DLF414 <Dmitry220198@gmail.com>
2018-05-21 04:32:32 +0300

    update

--- helpers/error.js
+++ helpers/error.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-express.response.error = function(error) {
-    if(!error.code){
+express.response.error = function (error) {
+    if (!error.code) {
         error = {
             message: error.toString(),
             code: 'server_error',
@@ -15,23 +15,82 @@ express.response.error = function(error) {
 module.exports = {
     invalidId: {
         message: 'Invalid id',
-        code: 'invalid_id', status: 400 },
+        code: 'invalid_id',
+        status: 400
+    },
     notFound: {
         message: 'Entity not found',
-        code: 'entity_not_found', status: 404 },
+        code: 'entity_not_found',
+        status: 404
+    },
     wrongCredentials: {
         message: 'Email or password are wrong',
-        code: 'wrong_credentials', status: 404 },
+        code: 'wrong_credentials',
+        status: 404
+    },
     accessDenied: {
         message: 'Access denied',
-        code: 'access_denied', status: 403 },
-    validationError: {
-        message: 'Wrong input data',
-        code: 'wrong_data', status: 400 },
-    loginExist: {
-        message: 'This login is exists',
-        code: 'login_exists', status: 400 },
-    chatExist: {
-        message: 'This chat is exists',
-        code: 'chat_exists', status: 400 },
-}
+        code: 'access_denied',
+        status: 403
+    },
+    createError: {
+        message: 'Invalid data',
+        code: 'invalid_data',
+        status: 400
+    },
+    optionsError: {
+        message: 'Invalid options',
+        code: 'invalid_options',
+        status: 400
+    },
+    AccountAlreadyExists: {
+        message: 'The specified account already exists.',
+        code: 'AccountAlreadyExists',
+        status: 409
+    },
+    AccountBeingCreated: {
+        message: 'The specified account is in the process of being created.',
+        code: 'AccountBeingCreated',
+        status: 409
+    },
+    AccountIsDisabled: {
+        message: 'The specified account is disabled.',
+        code: 'AccountIsDisabled',
+        status: 403
+    },
+    InsufficientAccountPermissions: {
+        message: 'The account being accessed does not have sufficient permissions to execute this operation.',
+        code: 'InsufficientAccountPermissions',
+        status: 403
+    },
+    AuthenticationFailed: {
+        message: 'Server failed to authenticate the request.',
+        code: 'AuthenticationFailed',
+        status: 403
+    },
+    InvalidQueryParameterValue: {
+        message: 'An invalid value was specified for one of the query parameters in the request URI.',
+        code: 'InvalidQueryParameterValue',
+        status: 400
+    },
+    InvalidUri: {
+        message: 'The requested URI does not represent any resource on the server.',
+        code: 'InvalidUri',
+        status: 400
+    },
+    InternalError: {
+        message: 'The server encountered an internal error.',
+        code: 'InternalError',
+        status: 500
+    },
+    InvalidInput: {
+        message: 'One of the request inputs is not valid.',
+        code: 'InvalidInput',
+        status: 400
+    },
+    MissingRequiredQueryParameter: {
+        message: 'A required query parameter was not specified for this request.',
+        code: 'MissingRequiredQueryParameter',
+        status: 400
+    }
+};
--- services/crud.js
+++ services/crud.js
@@ -1,43 +1,47 @@
-'use strict';
-
-class CrudService{
-    constructor(repository, errors){
+class CrudService {
+    constructor(repository, errors) {
         this.repository = repository;
         this.errors = errors;
 
         this.defaults = {
             readChunk: {
                 limit: 10,
-                page: 1,
-                order: 'asc',
-                orderField: 'id'
+                offset: 0,
+                sortOrder: 'asc',
+                sortField: 'created'
             }
-        }
+        };
     }
 
-    async readChunk(options){
-        options = Object.assign({}, this.defaults.readChunk, options);
+    async readChunk(options) { options = Object.assign(
+        {}, this.defaults.readChunk, options
+    );
 
-        let limit = options.limit;
-        let offset = (options.page - 1) * options.limit;
+        let limit = Number.parseInt(options.limit);
+        let offset = Number.parseInt(options.offset);
 
         return await this.repository.findAll({
             limit: limit,
             offset: offset,
-            order: [[options.orderField, options.order.toUpperCase()]],
+            order: [[
+                options.sortField,
+                options.sortOrder.toUpperCase()
+            ]],
             raw: true
         });
     }
 
-    async read(id){
+    async read(id) {
         id = parseInt(id);
 
         if (isNaN(id)) {
             throw this.errors.invalidId;
         }
 
-        const item = await this.repository.findById(id);
-
+        const item = await this.repository.findById(
+            id,
+            { raw: true }
+        );
         if (!item) {
             throw this.errors.notFound;
         }
@@ -45,20 +49,25 @@ class CrudService{
         return item;
     }
 
-    async create(data){
+    async create(data) {
         const item = await this.repository.create(data);
 
         return item.get({ plain: true });
     }
 
-    async update(id, data){
-        await this.repository.update(data, { where: { id: id }, limit: 1 });
+    async update(id, data) {
+        await this.repository.update(data, {
+            where: { id: id },
+            limit: 1
+        });
 
         return this.read(id);
     }
 
-    async delete(id){
-        return this.repository.destroy({ where: { id: id } });
+    async delete(id) {
+        return this.repository.destroy({
+            where: { id: id }
+        });
     }
 }
 
